d507ab668994d19a803e68f0ebdc505419573bc2
joseah <joseah@lcg.unam.mx>
2016-03-12 02:28:14 -0600

    Create html file with pypandoc

--- JStoMarkdown.py
+++ JStoMarkdown.py
@@ -18,8 +18,8 @@ import sys
 # Import "re" library for using regular expressions
 import re
 
+# Import pandoc wrapper 
 import pypandoc
-from  subprocess import call
 
 data = sys.argv[1] # js file
 
@@ -31,6 +31,7 @@ filename = sys.argv[1].replace(".js", "")
 output = open(filename + ".md", "w")
 
 
+md = []
 # Open file via a connection
 file = open(data, 'r')
 for l in file:
@@ -44,43 +45,43 @@ for l in file:
 
          # If we are within a comment, print all lines
         if(comment == 1):
-            print >> output, l
+             md.append(l)
 
         # If a comment begins, indicate that there is a comment
         if(comment_begins != None):
-        	comment = 1
+            comment = 1
 
         # If a comment ends, indicate that a comment has finished
         if(comment_ends != None):
-        	comment = 0
+            comment = 0
 
         # If a comment has not started and has not ended it means that there is a chunk of code. 
         # Print markdown code label and indicate that code has started
         if(comment == 0 and code == 0):
             if(l != '' and l != "*/"):
-                print >> output, "\n```js"
+                md.append("\n```js")
                 code = 1
 
         # If a comment has not started and has not ended it and there is code we want to print all lines
         if(comment == 0 and code):
-        	if(l != '' and l != "*/"):
-        	    print >> output, l
+            if(l != '' and l != "*/"):
+                md.append(l)
 
         # If a comment has started it means that the chunk of code has finished. End chunk of code and indicate
         # that there is no code anymore.
         if(comment_begins != None and code):
-            print >> output, "```"
+            md.append("```")
             code = 0
       
 file.close()
 
 if(comment == 0):
-    print >> output, "```",
+    md.append("```")
 
-# print "pandoc " + filename + ".md" + " -o " + filename + ".html"
-# pandoc functions.md -o  functions.html 
-# call("pandoc " + filename + ".md" + " -o " + filename + ".html", shell = True)
-# subprocess.check_call(["pandoc ", filename, ".md", " -o ", filename , ".html"], shell = True)
+md = '\n'.join(md)
 
-# output = pypandoc.convert("# This is a test", 'html', format = "md")
-# assert output == ""
+html = pypandoc.convert(md, 'html', format = "md")
+
+x = open(filename + ".html", "w")
+print >> x, html
+x.close()
