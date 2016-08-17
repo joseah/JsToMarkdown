da1e1d0cf4fc3870c82290ecddcaade2d8ed36d0
Jose Alquicira H <joseah@lcg.unam.mx>
2016-08-17 16:11:05 -0500

    sets indentation to 4 spaces

--- PythontoMarkdown.py
+++ PythontoMarkdown.py
@@ -52,6 +52,7 @@ args = parser.parse_args()
 #' Flag variables
 comment = 0
 code = 0
+skip = 0
 
 #' Open output markdown file
 filename = args.s.replace(".py", "")
@@ -63,47 +64,48 @@ file = open(args.s, 'r')
 #' Convert script to markdown format 
 
 for l in file:
-        l = l.strip('\n')
-        md_comm =  re.match(".*^[#]{1}[']{1}.*", l)
-        comment_begins  =  l.find("'''#")
-        comment_ends  =  l.find("'''")
+    skip = 0
+    l = l.strip('\n')
+    md_comm =  re.match(".*^[#]{1}[']{1}.*", l)
+    comment_begins  =  l.find("'''#")
+    comment_ends  =  l.find("'''")
 
-        # If a comment ends, indicate that a comment has finished
-        if(comment_ends != -1):
-            comment = 0
+    # If a comment ends, indicate that a comment has finished
+    if(comment_ends != -1):
+        comment = 0
 
         # If a comment begins, indicate that there is a comment
-        if(comment_begins != -1):
-            comment = 1
+    if(comment_begins != -1):
+        comment = skip = 1
 
         # If we are within a comment, print all lines
-        if(comment == 1):
-            if l.find("'''#") == -1:
-                 md.append(l)
+    if(comment == 1):
+        if l.find("'''#") == -1:
+            md.append(l)
+ 
+    #' If a comment has started it means that the chunk of code has finished. End chunk of code and indicate
+    #' that there is no code anymore.
+    if(md_comm != None and code and comment == 0):
+        md.append("```")
+        code = 0
 
-        #' If a comment has started it means that the chunk of code has finished. End chunk of code and indicate
-        #' that there is no code anymore.
-        if(md_comm != None and code and comment == 0):
-            md.append("```\n")
-            code = 0
+    #' If we are within a markdown comment, format line and append
+    if(md_comm and comment == 0):
+        l_format = re.sub("#'\s*", '', l)
+        md.append(l_format)
 
-        #' If we are within a markdown comment, format line and append
-        if(md_comm and comment == 0):
-            l_format = re.sub("#'\s*", '', l)
-            md.append(l_format)
+    #' If a comment has not started it means that there is a chunk of code. 
+    #' Print markdown code label and indicate that code has started
 
-        #' If a comment has not started it means that there is a chunk of code. 
-        #' Print markdown code label and indicate that code has started
+    if(md_comm == None and code == 0 and comment == 0):
+        if(l != ''):
+            md.append("\n```python")
+            code = 1    
 
-        if(md_comm == None and code == 0 and comment == 0):
-            if(l != ''):
-                md.append("\n```python")
-                code = 1    
-
-        #' If a comment has not started, there is code we want to append
-        if(md_comm == None and code and comment == 0):
-            if(l != ''):
-                md.append(l)
+    #' If a comment has not started, there is code we want to append
+    if(md_comm == None and code and comment == 0):
+        if(l != ''):
+            md.append(l)
 
       
 file.close()
