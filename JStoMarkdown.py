a6737372433acf2a2b52b7a95f5c45f672334dd1
Jose Alquicira H <joseah@lcg.unam.mx>
2016-08-17 16:35:25 -0500

    uses markdown style for comments

--- JStoMarkdown.py
+++ JStoMarkdown.py
@@ -23,16 +23,16 @@
 
 
 
-# Import "sys" library for managing command parameters
+#' Import "sys" library for managing command parameters
 import sys
 
-# Import "re" library for using regular expressions
+#' Import "re" library for using regular expressions
 import re
 
-# Import "io" to deal with text enconding
+#' Import "io" to deal with text enconding
 import io
 
-# Import pandoc wrapper
+#' Import pandoc wrapper
 
 ###############################
 ## Install pypandoc via:      #  
@@ -41,7 +41,7 @@ import io
 
 import pypandoc
 
-# Import "argparse" to handle command-line arguments
+#' Import "argparse" to handle command-line arguments
 import argparse
 
 parser = argparse.ArgumentParser(description='Gets parameters.')
@@ -50,52 +50,52 @@ parser.add_argument("-o", required=True)
 parser.add_argument("-c", required=False)
 args = parser.parse_args()
 
-# Flag variables
+#' Flag variables
 comment = 0
 code = 0
 
-# Open output markdown file
+#' Open output markdown file
 filename = args.s.replace(".js", "")
 
 md = []
-# Open file via a connection
+#' Open file via a connection
 file = open(args.s, 'r')
 for l in file:
         l = l.strip('\n')
         comment_begins  =  re.match(".*[/]+[*]+.*", l)
         comment_ends  =  re.match(".*[*]+[/]+.*", l)
         
-        # If a comment ends, indicate that a comment has finished
+        #' If a comment ends, indicate that a comment has finished
         if(comment_ends != None):
             comment = 0
 
-         # If we are within a comment, print all lines
+        #' If we are within a comment, print all lines
         if(comment == 1):
              md.append(l)
 
-        # If a comment begins, indicate that there is a comment
+        #' If a comment begins, indicate that there is a comment
         if(comment_begins != None):
             comment = 1
 
-        # If a comment ends, indicate that a comment has finished
+        #' If a comment ends, indicate that a comment has finished
         if(comment_ends != None):
             md.append('\n')
             comment = 0
 
-        # If a comment has not started and has not ended it means that there is a chunk of code. 
-        # Print markdown code label and indicate that code has started
+        #' If a comment has not started and has not ended it means that there is a chunk of code. 
+        #' Print markdown code label and indicate that code has started
         if(comment == 0 and code == 0):
             if(l != '' and l != "*/"):
                 md.append("\n```js")
                 code = 1
 
-        # If a comment has not started and has not ended it and there is code we want to print all lines
+        #' If a comment has not started and has not ended it and there is code we want to print all lines
         if(comment == 0 and code):
             if(l != '' and l != "*/"):
                 md.append(l)
 
-        # If a comment has started it means that the chunk of code has finished. End chunk of code and indicate
-        # that there is no code anymore.
+        #' If a comment has started it means that the chunk of code has finished. End chunk of code and indicate
+        #' that there is no code anymore.
         if(comment_begins != None and code):
             md.append("```")
             code = 0
@@ -105,15 +105,16 @@ file.close()
 if(comment == 0 and code):
     md.append("```")
 
-# Join list of lines
+#' Join list of lines
 md = '\n'.join(md)
 
-# Write raw markdown file
+#' Write raw markdown file
 md_file = open(filename + ".md", "w")
-print >> md_file, ''.join(md)
+md_file.write(''.join(md))
 md_file.close()
 
-# Convert markdown to output format
+
+#' # Convert markdown to output format
 
 if args.c:
     output_file = pypandoc.convert(md, args.o, format = "md", extra_args=['-c' + args.c, '--toc', '-N'])
@@ -121,7 +122,7 @@ else:
     output_file = pypandoc.convert(md, args.o, format = "md")
 
 
-# Write html output
+#' # Write html output
 output = io.open(filename + "." + args.o, "w", encoding='utf8')
 output.write(output_file)
 output.close()
