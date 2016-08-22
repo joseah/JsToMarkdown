b53a17698e49fcbbe96f17fa48b869e5e465c67f
Jose Alquicira H <joseah@lcg.unam.mx>
2016-08-22 17:05:51 -0500

    adds SrctoMarkdown.py script

---
+++ SrctoMarkdown.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+#' ---
+#' title:        source2markdown
+#' author:       Jose Alquicira Hernandez
+#' ---
+
+#'
+#' # Description
+#' 
+#' This program takes a script written in **python**, **perl**, **javascript** or **shell**
+#' and generates a markdown document with comments processed as markdown and code embedded in
+#' markdown code tags.
+#'
+
+
+#' # Parameter description
+
+#'
+#' | Parameters |                  Description                  |
+#' |:----------:|:---------------------------------------------:|
+#' | -s         | script file (with comments in markdown style) |
+#' | -o         |              output format (html)             |
+#' | -c         |                    css file                   |
+#'
+
+#'
+#' # Usage example
+#'
+#' ```shell
+#' python SrctoMarkdown.py -s functions.js -o html -c kult.css 
+#' ```
+#'
+
+
