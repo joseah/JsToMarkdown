11292f5b57ed7888d8b83224d466594ebe2b72ac
Jose Alquicira H <joseah@lcg.unam.mx>
2016-09-01 13:40:32 -0500

    Documents functions; returns a list in group function

--- PythontoMarkdown.py
+++ PythontoMarkdown.py
@@ -21,17 +21,36 @@
 #'
 
 
-import re
+#' # Define functions
+
+'''#
+## format_single_comment
+
+### Description
+
+This function takes a list where each item corresponds to one single line of 
+the script file. All items are order according to original script.
+
+### Parameters
+
+| Parameters |                  Description                              |
+|:----------:|:---------------------------------------------------------:|
+| script     |     list corresponding to script file                     |
+| lang       | programming language tag: python, perl, shell, javascript |
 
 
-#' Open file via a connection
-file = open("/Users/joseah/Documents/lab_collado/github/SrcToMarkdown/test.py", 'r')
-script = map(str.strip,file.readlines())
-file.close()
+### Usage example 
 
-markdown = []
+```python
+format_single_comment(file, "python")
+```
 
-#' Convert script to markdown format 
+### Output
+
+- `md`: a list corresponding to script file with formatted single-line comments 
+and code tags added to code chunks.
+
+#'''
 
 def format_single_comment(script, lang):
     md = []
@@ -67,7 +86,39 @@ def format_single_comment(script, lang):
 
     if prev_line_code:
         md.append("```")
-    return(md)    
+    return(md) 
+
+
+#' ---
+
+
+'''#
+## group
+
+### Description
+
+Splits a list according to a provided string pattern.
+
+
+### Parameters
+
+| Parameters |                  Description                              |
+|:----------:|:---------------------------------------------------------:|
+| seq        |     list corresponding to script file                     |
+| sep        |    pattern used to split the list                         |
+
+
+### Usage example 
+
+```python
+script_split = group(script, "\'\'\'#")
+```
+
+### Output
+
+- `g`: a list of lists separated by provided string pattern
+
+#'''
 
 def group(seq, sep):
     g = []
@@ -76,9 +127,31 @@ def group(seq, sep):
             yield g
             g = []
         g.append(el)
-    yield g
+    yield list(g)
+
+
+
+#' # Code starts
+
+#' ## Import libraries
+
+import re
+
+
+#' Open file via a connection
+file = open("/Users/joseah/Documents/lab_collado/github/SrcToMarkdown/test.py", 'r')
+script = map(str.strip,file.readlines())
+file.close()
+
+markdown = []
+
+#' Convert script to markdown format 
+
+   
+
+
     
-script_split = list(group(script, "'''#"))
+script_split = group(script, "'''#")
 
 
 for l in script_split:
@@ -86,19 +159,17 @@ for l in script_split:
 
     if len(i) > 1:
         res = [x for x in i[0] if x != "'''#"]
-        #print(*res, sep="\n")
         markdown.extend(res)
         
         res_2 = [x for x in i[1] if  x != "#'''"]
         res_2 = format_single_comment(res_2, "python")
         markdown.extend(res_2)
-        #print(*res_2, sep="\n")
+
         
     else:
         res_3 = format_single_comment(i[0], "python")
         markdown.extend(res_3)
-        # print(*i[0], sep="\n")
 
     
 
-#print(*markdown, sep="\n")
+print(*markdown, sep="\n")
