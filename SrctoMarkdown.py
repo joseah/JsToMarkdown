ec0e082569a1366feb9f421672da55292ccc2a9c
Jose Alquicira H <joseah@lcg.unam.mx>
2016-09-21 16:59:22 -0500

    integrates various programming languages; fixes some comments

--- SrctoMarkdown.py
+++ SrctoMarkdown.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 
-'''
+'''#
+
 ---
-title:        source2markdown
-author:       Jose Alquicira Hernandez
+title: SrctoMarkdown.py
+subtitle: Convert script to markdown document
+author: Jose Alquicira Hernandez
 ---
 
 
 # Description
  
-This program takes a script written in **python**, **perl**, **javascript** or **shell**
+This program takes a script written in **python**, **perl**, **javascript**, **R** or **shell**
 and generates a markdown document with comments processed as markdown and code embedded in
 markdown code tags.
 
@@ -51,31 +53,244 @@ sudo pip install pypandoc
 python SrctoMarkdown.py -s functions.js -o html -c kult.css 
 ```
 
-'''
+#'''
 
-#' # Program starts
+#' # Define functions
 
-#' Import `sys` library for managing command parameters
-import sys
+'''#
+## format_single_comment
 
-#' Import `re` library for using regular expressions
+### Description
+
+This function takes a list where each item corresponds to one single line of 
+the script file. All items are order according to original script.
+
+### Parameters
+
+| Parameters |                  Description                              |
+|:----------:|:---------------------------------------------------------:|
+| script     |     list corresponding to script file                     |
+| lang       | programming language tag: python, perl, shell, javascript |
+
+
+### Usage example 
+
+```python
+format_single_comment(file, "python")
+```
+
+### Output
+
+- `md`: a list corresponding to script file with formatted single-line comments 
+and code tags added to code chunks.
+
+#'''
+
+def format_single_comment(script, lang):
+    md = []
+    prev_line_code = False
+    
+    for l in script:
+        l = l.strip('\n')
+        md_comm = re.match("#'", l)
+        
+        # Currrent line is a comment
+        if md_comm != None:
+            # Previous line was code
+            if prev_line_code:
+                md.append("```\n")
+            # Previous line was not code
+            if l != '':
+                l_format = re.sub("#'\s*", '', l)
+                md.append(l_format)
+            
+        # Current line is not a comment        
+        else:
+            # Previous line was not code
+            if not prev_line_code and l != '':
+                md.append("\n```" + lang)
+                md.append(l)
+            # Previous line was code
+            elif l != '':
+                md.append(l)
+                
+        if md_comm != None:
+            prev_line_code = False
+        elif l != '':
+            prev_line_code = True
+
+    if prev_line_code:
+        md.append("```")
+    return(md) 
+
+
+#' ---
+
+
+'''#
+## group
+
+### Description
+
+Splits a list according to a string pattern.
+
+
+### Parameters
+
+| Parameters |                  Description                              |
+|:----------:|:---------------------------------------------------------:|
+| seq        |     list corresponding to script file                     |
+| sep        |    pattern used to split the list                         |
+
+
+### Usage example 
+
+```python
+script_split = group(script, "\'''#")
+```
+
+### Output
+
+- `g`: a list of lists separated by provided string pattern
+
+#'''
+
+def group(seq, sep):
+    g = []
+    for el in seq:
+        if re.match(sep, el) != None :
+            yield g
+            g = []
+        g.append(el)
+    yield list(g)
+
+
+
+'''#
+## format_multiple_line_comment
+
+### Description
+
+This function takes a list where each item corresponds to one single line of 
+the script file. All items are ordered according to original script.
+
+### Parameters
+
+| Parameters |                  Description                              |
+|:----------:|:---------------------------------------------------------:|
+| script     |     list corresponding to script file                     |
+| lang       | programming language tag: python, perl, shell, javascript |
+| comment_tag_start | Tag to indicate markdown multiple-line comment start(\'\'\'# or /*) |
+| comment_tag_end | Tag to indicate markdown multiple-line comment end (#\'\'\' or */) |
+
+### Usage example 
+
+```python
+format_single_comment(file, "python")
+```
+
+### Output
+
+- `markdown`: a list corresponding to script file with formatted single-line 
+comments, multiple-line comments and code tags added to code chunks.
+
+#'''
+
+
+
+def format_multiple_line_comment(script, lang, 
+                                 comment_tag_start, comment_tag_end):
+
+    markdown = []
+    script_split = group(script, comment_tag_start)
+
+    for l in script_split:
+        i = list(group(l,comment_tag_end))
+        if len(i) > 1:
+            res = [re.sub("^\s*", '', x).rstrip("\n") for x in i[0] if re.match(comment_tag_start, x) == None ]
+            markdown.extend(res)
+
+            
+            res_2 = [x for x in i[1] if  re.match(comment_tag_end, x) == None]
+            res_2 = format_single_comment(res_2, lang)
+            markdown.extend(res_2)
+            
+        else:
+            res_3 = format_single_comment(i[0], lang)
+            markdown.extend(res_3)
+    
+    return(markdown)
+    
+
+#' # Code starts
+
+#' ## Import libraries
+
+#' Import `re` to use regular expressions
 import re
-
-#' Import `io to deal with text enconding
-import io
-
-# Import pandoc wrapper
-
-
-
-import pypandoc
 
 #' Import `argparse` to handle command-line arguments
 import argparse
 
+#' Import `io` to deal with text enconding
+import io
+
+#' Import pandoc wrapper
+import pypandoc
+
+#' Set path to pandoc
+import os
+os.environ.setdefault('PYPANDOC_PANDOC', '/usr/local/bin/pandoc')
+
+#' Get parameters 
 parser = argparse.ArgumentParser(description='Gets parameters.')
 parser.add_argument("-s", required=True)
 parser.add_argument("-o", required=True)
 parser.add_argument("-c", required=False)
 args = parser.parse_args()
 
+#' Get filename and language program
+script_info = args.s.split(".")
+filename = script_info[0]
+extension = script_info[1]
+
+
+#' Open file
+file = open(args.s, 'r')
+script = file.readlines()
+file.close()
+
+#' Convert script to markdown format 
+
+if extension == "pl":
+    md_doc = format_single_comment(script, extension)
+elif extension == "sh":
+    md_doc = format_single_comment(script, "bash")
+elif extension == "R" or extension == "r":
+    md_doc =  format_single_comment(script, "R")
+elif extension == "py":
+    md_doc = format_multiple_line_comment(script, "python", "'''#", "#'''")
+elif extension == "js":
+    md_doc = format_multiple_line_comment(script, "js", "/**", "**/")
+
+
+#' Join list of lines
+md = '\n'.join(md_doc)
+
+#' Write raw markdown file
+md_file = open(filename + ".md", "w")
+md_file.write(''.join(md))
+md_file.close()
+
+#' # Convert markdown to output format
+
+if args.c:
+    output_file = pypandoc.convert_text(md, args.o, format = "md", extra_args=['-c' + args.c, '--toc', '-N', '--self-contained', '--standalone'])
+else:
+    output_file = pypandoc.convert_text(md, args.o, format = "md")
+
+
+#' # Write html output
+output = io.open(filename + "." + args.o, "w", encoding='utf8')
+output.write(output_file)
+output.close()
